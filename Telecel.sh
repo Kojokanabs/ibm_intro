f11908f51afc00d010b9d4044babd6a8e1ff4b00
Enoch K. Ali <Kojokanabs@gmail.com>
2024-07-30 23:33:17 +0000

    DE_introduction

---
+++ Telecel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "My network is bought from Vodafone ghana"
+echo	Who is the ypoung man in Sakumon residence?
+read Name
+echo What about the youngest lady is the house
+read name
+printf "Are %s and %s siblings." "$Name" "$name"
